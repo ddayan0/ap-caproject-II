65e09041699e77b433bc35e88fe51fa555a6a384
ddayan0 <denday198@gmail.com>
2020-06-09 12:32:03 -0400

    build, gitignore

---
+++ build/precache-manifest.0d23b8dec23dc7c35abb6faab2738854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b393d304877bf24c6e25bba53f3ce65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43ec40e3911ce2befb35",
+    "url": "./static/css/2.e603e8eb.chunk.css"
+  },
+  {
+    "revision": "f2f21a5524954df8c1db",
+    "url": "./static/css/main.43e64783.chunk.css"
+  },
+  {
+    "revision": "43ec40e3911ce2befb35",
+    "url": "./static/js/2.8ebcd146.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8ebcd146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f21a5524954df8c1db",
+    "url": "./static/js/main.1c43cc0f.chunk.js"
+  },
+  {
+    "revision": "cf4d756d0797c951bbb5",
+    "url": "./static/js/runtime-main.e118c5fc.js"
+  }
+]);
